ef74edf6ae20624f8c5886cf028b8e1c5e92b5ed
PranavDeepakSathya <sathya.pranav.deepak@gmail.com>
2026-02-20 02:12:00 +0530

    Initialize virtual environment and install packages
    Set up Python virtual environment and install dependencies.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+python3 -m venv venv
+source venv/bin/activate
+
+pip install --upgrade pip setuptools wheel
+pip install nvcc4jupyter numpy jupyter_client ipykernel ipywidgets
+pip install triton
+pip install torch
+pip install matplotlib
+pip install pandas
+
+git config --global user.email sathya.pranav.deepak@gmail.com
+git config --global user.name PranavDeepakSathya
