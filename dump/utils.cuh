b0921f7867ee8e317087c0e249008e58d306cd51
PranavDeepakSathya <sathya.pranav.deepak@gmail.com>
2026-02-20 00:13:00 +0000

    just_added_some_utils_still_experiments_stage

--- dump/utils.cuh
+++ dump/utils.cuh
@@ -25,3 +25,24 @@
         exit(1); \
     } \
 }
+
+__device__ inline
+int elect_sync() {
+  int pred = 0;
+  asm volatile(
+    "{\n"
+    ".reg .pred P;\n"
+    "elect.sync _|P, %1;\n"
+    "@P mov.s32 %0, 1;\n"
+    "}"
+    : "+r"(pred) : "r"(0xFFFF'FFFF)
+  );
+  return pred;
+}
+
+template <typename T>
+__device__ inline
+T warp_uniform(T x) {
+  return __shfl_sync(0xFFFF'FFFF, x, 0);
+}
+
